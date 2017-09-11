bf12b1694316bb2dd427970943eef8f0e2f88b1c
George Tourtsinakis <george@besmartbesimple.com>
2017-09-11 20:09:39 +0300

    Random class

--- Itirations/Itirations/Program.cs
+++ Itirations/Itirations/Program.cs
@@ -12,8 +12,9 @@ namespace Itirations
         {
 
             //An itiration is a loop that runs until a condition met
-            //It's usually used so you can have the data of array like 
+            //It's usually used so you can have the data of an array  or a lot of data
 
+            #region for loop
             //The first value is like the start we set i, next we are saying if i<= 10 then i++
             for (var i = 0; i <= 10; i++) {
 
@@ -27,18 +28,91 @@ namespace Itirations
             }
 
 
-
+            //Reverse order
             for (var i = 10; i >= 1; i--)
             {
 
-
-                //The % operator computes the remainder after dividing its first operand by its second
                 if (i % 2 == 0)
                 {
                     Console.WriteLine(i);
                 }
 
             }
+
+            #endregion
+
+
+
+
+
+            #region foreach loop
+
+            var name = "Bee";
+
+            //A string is an array of characters. Length give as the total of the array and 
+            //we are looping each index in it
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //The same more readable like for above
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            //Looping throu an array
+            string[] fruits = new string[] { "banana","apple","melon"};
+
+            foreach (var fruit in fruits)
+            {
+
+                Console.WriteLine(fruit);
+
+            }
+
+            #endregion
+
+
+
+
+            #region while loop
+
+            var k = 0;
+
+            while (k<10) {
+
+                k++;
+                Console.WriteLine(k);
+
+            }
+
+
+            while (true) {
+
+                Console.Write("Input your name");
+
+                var input = Console.ReadLine();
+
+                //Checking if string is not empty
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo:" + input);
+                    continue;//The loops continues and starts again from while
+                }
+                else
+                {
+                    break;
+                }
+                
+
+
+            }
+
+            #endregion
+
+
 
         }
     }
---
+++ Random_Class/Random_Class/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //The Random Class exists in .net framework so we can generate random numbers
+            var random = new Random();
+
+            const int passwordLength = 10;
+
+            var buffer = new char[passwordLength];
+
+
+            for (var i = 0; i < passwordLength; i++)
+            {
+                
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            }
+
+            var password = new string(buffer);
+                
+                
+                //Generating random  alphabet character
+                Console.WriteLine((char)('a' + random.Next(0,26))); 
+
+                Console.WriteLine(password);
+
+
+            var dice = random.Next(1, 20);
+            Console.WriteLine("You rolled a " + dice);
+           
+            
+            //ASCII code
+            //Console.WriteLine((int)'a'); //Returns 97 which is his ascii code
+
+
+
+        }
+    }
+}
