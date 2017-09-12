e4a5bd7d2db1a95fc3e256601fe4e7c0a6329ce3
George Tourtsinakis <george@besmartbesimple.com>
2017-09-12 17:04:42 +0300

    Finished itiration excrcises
    - Added some info about multi dimensional arrays and some array methods/properties

---
+++ Itirations_Exercises/Itirations_Exercises/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itirations_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Run here to see the method you want to check how it works. For example
+            biggerNumber();
+
+        }
+
+        public static void divisible()
+        {
+            //Write a program to count how many numbers between 
+            //1 and 100 are divisible by 3 with no remainder.
+            //Display the count on the console.
+
+
+            var count = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    count++;
+            }
+
+            Console.WriteLine("The numbers that can be divided by 3 are {0}", count);
+
+        }
+
+        //Write a program and continuously ask the user to enter a number or "ok" to exit. 
+        //Calculate the sum of all the previously entered numbers 
+        //and display it on the console.
+        public static void unlimitedCount() {
+            
+            var sum = 0;
+
+            while (true)
+            {
+
+                Console.WriteLine("Enter a number or write ok to exit");
+                var input = Console.ReadLine();
+
+                if (input == "ok")
+                {
+                    break;
+                }
+                else
+                {
+                    sum += Convert.ToInt32(input);
+                }
+                Console.WriteLine("Sum of numbers is  {0}", sum);
+            }
+        }
+
+
+
+        //    Write a program which takes a single argument from the console, computes the factorial and prints the
+        ///// value on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 
+        ///// and display it as 5! = 120.
+        public static void factorial() {
+
+            Console.WriteLine("Write a number and I will give you the factorial");
+            var inputNumber = Convert.ToInt32(Console.ReadLine());
+            int result = inputNumber;
+
+            for (int i = 1; i < inputNumber; i++)
+            {
+                result = result * i;
+
+            }
+
+            Console.WriteLine(result);
+
+        }
+
+
+
+
+
+        //Write a program that picks a random number between 1 and 10.Give the user 4 chances to guess the number.
+        //If the user guesses the number, display “You won"; otherwise, display “You lost". 
+        //(To make sure the program is behaving correctly, you can display the secret number on the console first.)
+        public static void guessNumber() {
+
+            var random = new Random();
+
+            var randomNumber = random.Next(1, 10);
+
+            Console.WriteLine(randomNumber);
+            Console.WriteLine("Guess a number");
+
+            var totalTries = 0;
+
+            while (totalTries <= 4)
+            {
+                var pickedNumber = Convert.ToInt32(Console.ReadLine());
+                if (randomNumber == pickedNumber)
+                {
+                    
+                    totalTries = 4;
+                    Console.WriteLine("Correct number");
+                    break;
+                }
+                else if (randomNumber != pickedNumber)
+                {
+
+                    totalTries++;
+                    Console.WriteLine("Wrong number");
+                    continue;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the 
+        /// numbers and display it on the result. For example, if the user enters “5, 3, 8, 1, 4", the program should 
+        /// display 8 on the console.
+        /// </summary>
+        public static void biggerNumber()
+        {
+
+
+
+
+            Console.WriteLine("Enter comma seperated numbers");
+            var input = Console.ReadLine();
+
+            var numbers = input.Split(',');
+
+            //Just declare the fake max that will change later according to biggest number
+            var max = 0;
+
+            foreach (var item in numbers)
+            {
+
+                var number = Convert.ToInt32(item);
+
+                if (number > max) {
+
+                    max = number;
+                    
+                }
+            }
+
+            Console.WriteLine("the biggest number is " + max);
+
+            
+
+
+
+        }
+
+
+
+
+
+    }//End Class
+}
---
+++ MultiDimensionalArrays/MultiDimensionalArrays/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDimensionalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In C# except single dimension we have multidimensional arrays
+            //They are used for structures such as matrix
+
+
+            //This is a multidimensional array means it will have 3 rows of 5 columns each
+
+            //Rectangular has same columns iin each row
+            var matrix = new int[3, 5]
+            {
+
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 }
+            };
+
+            //Jagged
+            var array = new int[3][];
+
+            array[0] = new int[4];
+            array[1] = new int[3];
+            array[2] = new int[2];
+
+
+            //The array type is a class and some predefined properties and methods are 
+            //Length
+            //Clear()
+            //Copy()
+            //IndexOf()
+            //Reverse()
+            //Sort()
+
+
+            int[] numbers = new int[] { 3, 7, 9, 14, 3 };
+
+            //Length property returns the size of the array
+            Console.WriteLine("Length of the array is" +numbers.Length);
+
+            //IndexOf return the index
+            var index = Array.IndexOf(numbers,9);
+            Console.WriteLine("Index of 9 is " + index);
+
+
+            //Clear() sets items specific value. First parameter is the array,second the value and third how many to change
+            Array.Clear(numbers, 0, 3);
+
+            Console.WriteLine();
+            Console.WriteLine("How clear works ");
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //Copy() copies array to another array
+            int[] numbers2 = new int[3];
+            Array.Copy(numbers, numbers2, 3);
+
+            Console.WriteLine();
+            Console.WriteLine("How copy works");
+            foreach (var item in numbers2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //Sort() It sorts by smaller to bigger
+            Array.Sort(numbers);
+
+            Console.WriteLine("How Sort() works");
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            //Reverse() it reverses the array from bigger to smaller
+            Array.Reverse(numbers);
+
+            Console.WriteLine("How Reverse() works");
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+    }
+}
