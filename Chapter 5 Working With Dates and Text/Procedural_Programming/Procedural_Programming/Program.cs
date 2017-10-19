c748225b736ff5cb3318af685d5038486440034d
George Tourtsinakis <george@besmartbesimple.com>
2017-10-19 15:50:09 +0300

    Procedural programming
    - Added procedural programming examples

---
+++ Chapter 5 Working With Dates and Text/Procedural_Programming/Procedural_Programming/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procedural_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Console.WriteLine("Write a name and I will reverse it");
+
+            var name = Console.ReadLine();
+            var reversedArray = ReverseName(name);
+
+            Console.WriteLine(reversedArray);
+
+            Console.WriteLine("Write numbers seperated with comma and i ll show you unique ones");
+
+            
+            var numbers = new List<int>() { 5, 4, 4, 3 };
+            
+            GetUniqueNumbers(numbers);
+            
+            
+        }
+
+
+        
+        
+        
+        //******METHODS***********
+
+
+        //- First we use the public keyword then the static so we can call it anywhere  directly
+        public static string ReverseName(string name)
+        {
+            char[] charArray = name.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            var reversed = string.Join("", charArray);
+
+            return reversed;
+
+
+            //OR we could write return string.Join("", charArray);
+        }
+
+
+        public static void GetUniqueNumbers(List<int> numbers)
+        {
+            var uniqueNumbers = new List<int>();
+           
+
+            uniqueNumbers = numbers.Distinct().ToList();
+            foreach (var uniqueNumber in uniqueNumbers)
+            {
+                Console.WriteLine(uniqueNumber);
+            }
+        }
+
+        
+
+
+    }
+}
---
+++ Chapter 5 Working With Dates and Text/Procedural_Programming/Procedural_Programming/readme.txt
@@ -0,0 +1,5 @@
+- In procedural programming we use methods to seperate code so we can reuse it and not repeat ourselves.
+- Here we get the exercise that we reverse the name and create method that's gonna reuse it anywhere we want, and a method that returns unique numbers.
+- Our application console is only used in console applications but the reverse array can be used anywhere or the unique numbers
+- so we need to seperate the logic.
+
